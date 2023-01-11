158a10cb95588f316c0a3242dadd2cb1cd1c001b
furkankayaa <kayaf.2000@gmail.com>
2023-01-11 22:22:26 +0300

    admin panel

---
+++ Backend/AspCoreMvc_App/Controllers/HomeController.cs
@@ -0,0 +1,241 @@
+﻿using App.Library;
+using AspCoreMvc_App.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AspCoreMvc_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpContextAccessor _httpContext;
+        //private readonly StudentDetailContext _context;
+
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor httpContext)
+        {
+            _logger = logger;
+            _httpContext = httpContext;
+        }
+
+
+        [TokenCheckFilter]
+        [HttpGet]
+        [Route("")]
+        [Route("[action]")]
+        public async Task<IActionResult> IndexAsync(string searchTerm)
+        {
+            //var allCategories = await GetRequest.GetCategoriesAsync(_httpContext);
+            //ViewBag.category = allCategories;
+            if (searchTerm != null)
+            {
+                ViewBag.category = searchTerm;
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+            //var token = HttpContext.Session.GetString("Token");
+            //if (string.IsNullOrEmpty(token))
+            //{
+            //    return RedirectToAction("Index", "UserData");
+            //}
+
+            //var response = await Helper.isValidAsync(token);
+
+            //if (response.IsSuccessStatusCode)
+            //{
+            //return View();
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Index", "UserData");
+            //}
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Search([FromQuery]string searchTerm)
+        {
+            var response = await GetRequest.GetApi($"https://e-gamestore.onrender.com/api/publishrequest/search?searchTerm={searchTerm}", _httpContext);
+            List<PublishRequestDetail> data = null;
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadFromJsonAsync<List<PublishRequestDetail>>();
+                
+            }
+            return RedirectToAction("Index", data);
+
+        }
+        
+        public async Task<IActionResult> Approve(int requestId)
+        {
+           
+            await PostRequest.PostApiAsync($"https://e-gamestore.onrender.com/api/publishrequest/approve?requestId={requestId}", _httpContext);
+            //var response = await PostRequest.PostApiAsync("http://localhost:5000/api/publishrequest/approve?requestId={requestId}", postData, _httpContext);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Reject(int requestId)
+        {
+           
+            await PostRequest.PostApiAsync($"https://e-gamestore.onrender.com/api/publishrequest/reject?requestId={requestId}", _httpContext);
+            //var response = await PostRequest.PostApiAsync("http://localhost:5000/api/publishrequest/reject?requestId={requestId}", postData, _httpContext);
+
+            return RedirectToAction("Index");
+        }
+        //[AllowAnonymous]
+        //[HttpGet]
+        //[Route("[action]/{categoryName}")]
+        //public IActionResult Index(string categoryName)
+        //{
+
+        //    var allCategories = GetRequest.GetCategories();
+        //    var category = allCategories.Where(x => x.CategoryName == categoryName).FirstOrDefault();
+        //    ViewBag.category = category;
+        //    return View(allCategories);
+        //}
+
+
+
+
+        //[Route("[action]")]
+        //public async Task<IActionResult> CartAsync([FromQuery(Name = "gameId")] int gameId)
+        //{
+        //    string userId = User.Identity.Name;
+        //    var cartItems = GetRequest.GetCartItems(userId);
+        //    var purchased = GetRequest.GetPurchases(userId);
+
+        //    //if (!HttpContext.Request.Cookies.ContainsKey(".AspNetCore.Identity.Application"))
+        //    //{
+        //    //string returnUrl = HttpContext.Request.Query["ReturnUrl"].ToString();
+        //    //return Redirect(returnUrl);
+        //    //return RedirectToAction("Index", "UserData", new { redirectUrl = redirectUrl, redirectCtrl = redirectCtrl, gameId = gameId });
+        //    //    return View();
+        //    //}
+        //    //else
+        //    //{
+        //    ViewBag.totalPrice = 0.00;
+        //    if (gameId != 0)
+        //    {
+        //        //While working on Docker container
+        //        var url = "http://cart.api/api/cart/post";
+
+        //        //While working on local
+        //        //var url = "http://localhost:5004/api/cart/post";
+
+
+        //        var game = GetRequest.GetGameById(gameId);
+        //        if (!Helper.isCartItem(game, cartItems) && !Helper.isPurchased(game, purchased))
+        //        {
+        //            CartItemDetail gameToAdd = new CartItemDetail
+        //            {
+        //                GameName = game.GameName,
+        //                GamePrice = game.GamePrice,
+        //                ImageUrl = game.ImageUrl,
+        //                Publisher = game.Publisher,
+        //                UserID = userId
+        //            };
+        //            await PostRequest.PostApiAsync(url, gameToAdd);
+        //            cartItems = GetRequest.GetCartItems(userId);
+        //        }
+
+        //        foreach (var item in cartItems)
+        //        {
+        //            ViewBag.totalPrice += item.GamePrice;
+        //        }
+        //        if (cartItems != null)
+        //            ViewBag.totalPrice = Math.Round(ViewBag.totalPrice, 2);
+        //        return View(cartItems);
+        //    }
+        //    else
+        //    {
+        //        foreach (var item in cartItems)
+        //        {
+        //            ViewBag.totalPrice += item.GamePrice;
+        //        }
+        //        if (cartItems != null)
+        //            ViewBag.totalPrice = Math.Round(ViewBag.totalPrice, 2);
+        //        return View(cartItems);
+        //    }
+        //    //}
+
+        //}
+
+        //[Route("[action]")]
+        //public async Task<IActionResult> CartRemoveAsync(int id)
+        //{
+        //    var userId = User.Identity.Name;
+        //    //While working on Docker container
+        //    var url = $"http://cart.api/api/cart/delete?id={id}&userId={userId}";
+
+        //    //While working on local
+        //    //var url = $"http://localhost:5004/api/cart/delete?id={id}&userId={userId}";
+
+
+        //    await PostRequest.DeleteApiAsync(url);
+
+        //    return RedirectToAction("Cart");
+        //}
+
+        //public async Task<IActionResult> CartRemoveAllAsync(string user)
+        //{
+        //    var userId = User.Identity.Name;
+
+        //    //While working on Docker container
+        //    var url = $"http://cart.api/api/cart/delete?userId={userId}";
+
+        //    //While working on local
+        //    //var url = $"http://localhost:5004/api/cart/delete?userId={userId}";
+
+        //    if (user == userId)
+        //        await PostRequest.DeleteApiAsync(url);
+
+        //    return RedirectToAction("Purchases");
+        //}
+
+
+        //[Route("[action]")]
+        //public IActionResult Purchases()
+        //{
+        //    string userId = User.Identity.Name;
+
+        //    var purchased = GetRequest.GetPurchases(userId);
+        //    return View(purchased);
+        //}
+
+        //[Route("[action]")]
+        //public async Task<IActionResult> CheckoutAsync()
+        //{
+        //    string userId = User.Identity.Name;
+        //    var cartItems = GetRequest.GetCartItems(userId);
+
+        //    //While working on Docker container
+        //    var url = "http://order.api/api/order/post";
+
+        //    //While working on local
+        //    //var url = "http://localhost:5006/api/order/post";
+
+        //    await PostRequest.PostApiAsync(url, cartItems);
+
+        //    return RedirectToAction("Purchases");
+        //}
+
+
+
+
+        //var redis = ConnectionMultiplexer.Connect("localhost:6379");
+        //var pubsub = redis.GetSubscriber();
+
+        //pubsub.Subscribe("order", (channel, message) => Console.WriteLine("ALINDI"));
+    }
+}
---
+++ Backend/AspCoreMvc_App/Startup.cs
@@ -0,0 +1,135 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using AspCoreMvc_App.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using App.Library;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace AspCoreMvc_App
+{
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        //public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddCors();
+
+            //services.AddDbContext<StudentDetailContext>();
+            //services.AddIdentity<AppUser, AppRole>(x =>
+            //{
+            //    x.Password.RequireUppercase = false;
+            //    x.Password.RequireNonAlphanumeric = false;
+            //    x.Password.RequiredLength = 2;
+            //}).AddEntityFrameworkStores<StudentDetailContext>();
+
+            //services.AddMvc(config =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder()
+            //                        .RequireAuthenticatedUser()
+            //                        .Build();
+            //    config.Filters.Add(new AuthorizeFilter(policy));
+
+            //});
+            //services.ConfigureApplicationCookie(options => { options.LoginPath = "/UserData";
+            //}) ;
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //        .AddCookie(options =>
+            //        {
+            //            options.LoginPath = "/UserData/";
+            //            //options.LogoutPath = "/UserData/Index/";
+            //        });
+
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            //services.AddScoped<IDbContextOptions, DbContextOptions>();
+            
+            //var mysqlConnectionString = Configuration.GetConnectionString("mysqlConnectionString");
+
+            //var host = Configuration["DBHOST"] ?? "localhost";
+            //var port = Configuration["DBPORT"] ?? "3306";
+            //var pw = Configuration["DBPASSWORD"] ?? "123";
+
+            //var mysqlConnectionString = $"server={host};userid=root;pwd={pw};" + $"port={port};database=Student-DB";
+
+            //services.AddDbContext<StudentDetailContext>(options =>
+            //    options.UseMySql(mysqlConnectionString, ServerVersion.AutoDetect(mysqlConnectionString), mySqlOptions =>
+            //    {
+            //        mySqlOptions.EnableRetryOnFailure();
+            //    }),
+            //    contextLifetime: ServiceLifetime.Transient,
+            //    optionsLifetime: ServiceLifetime.Singleton
+            //) ;
+
+
+            //services.AddDbContextPool<StudentDetailContext>(options =>
+            //{
+            //    options.UseMySql($"server={host};userid=root;pwd={pw};"
+            //      + $"port={port};database=Student-DB");
+            //});
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            //using (var scope =
+            //    app.ApplicationServices.CreateScope())
+            //{
+            //    var dbContext = scope.ServiceProvider.GetService<StudentDetailContext>();
+            //    dbContext.Database.Migrate();
+            //}
+
+
+            app.UseSession();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    //pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=UserData}/{action=Index}/");
+            });
+        }
+    }
+}
